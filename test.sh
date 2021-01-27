1c0485ee9774d97d4453d736d0e43bac1aac4247
Vebjørn Ostnes Gaupset <vebjorn.gaupset@bouvet.no>
2021-01-27 13:54:24 +0100

    Finishing commit

--- test.sh
+++ test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-echo "testing bash script in selfmade action"
-echo "Creating directory:"
+echo "Testing bash script in selfmade action"
+echo ""
+echo "Creating directory with name '$directoryName'..."
+echo ""
 mkdir "$directoryName"
+echo "Entering '$directoryName'..."
+echo ""
 cd $directoryName
-echo "You created a Director called: $directoryName"
-touch "${files[@]}"
-echo "You created these files inside '$directoryName':"
-ls
-read -p "Press enter to continue"
+echo "Creating file named '$fileName'..."
+echo ""
+cat $fileName > $fileName
+echo "Adding content to '$fileName'..." 
+echo $fileContent > $fileName
+echo ""
+echo "The content of the file is:"
+echo `cat $fileName`
+echo ""
