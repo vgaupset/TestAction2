d13010517540ecc8de052e6be243672fd73b02ed
Vebjørn Ostnes Gaupset <vebjorn.gaupset@bouvet.no>
2021-01-27 12:50:47 +0100

    files

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
 echo "testing bash script in selfmade action"
+echo "Creating directory:"
 mkdir "$directoryName"
+cd $directoryName
+echo "You created a Director called: $directoryName"
+touch "${files[@]}"
+echo "You created these files inside '$directoryName':"
 ls
+read -p "Press enter to continue"
